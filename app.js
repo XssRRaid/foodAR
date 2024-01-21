4f4accb99a4ed60012fa70d3c803cf7c94b7cbc9
Sarath <sarath.unnithan@gmail.com>
2024-01-22 05:01:41 +0530

    added login/register for admin, moved add-item to admin area

--- app.js
+++ app.js
@@ -6,32 +6,22 @@ const path = require('path')
 const multer = require('multer')
 
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf')
 
 const errorController = require('./controllers/error')
 const frontEndController = require('./controllers/frontEnd')
 
+const User = require('./models/user');
 
 // Requiring file system to use local files 
 const fs = require("fs");
 
-app.set('view engine', 'ejs')
-app.set('views', 'views')
-
-// const adminRoutes = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
-
-const adminRoutes = require('./routes/admin');
 const bodyParser = require('body-parser');
 
-
-app.use(bodyParser.urlencoded({extended: false}))
-
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.static(path.join(__dirname, 'data')))
-
-// app.use('/admin', adminRoutes);
-// app.use(shopRoutes);
+const app = express();
 
 
 // Set up multer storage and upload configuration
@@ -53,20 +43,64 @@ const storage1 = multer.diskStorage({
   }
 });
 
-// const uploadd = multer({ storage: storage1 });
-
-// const cpUpload = uploadd.fields([
-//   { name: 'itemImage', maxCount: 1 },
-//   { name: 'itemModel', maxCount: 1 }
-// ]);
-
 app.use(multer({ storage: storage1 }).fields([
     { name: 'itemImage', maxCount: 1 },
     { name: 'itemModel', maxCount: 1 }
   ])
 );
 
+// DB uri
+const MONGODB_URI =
+'mongodb+srv://thevagabondcoders:B2JsLpHGX0djP64V@cluster0.a6kahbs.mongodb.net/foodAR?retryWrites=true&w=majority';
+
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
+
+const csrfProtection = csrf()
+
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'data')))
+
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
+app.use(csrfProtection)
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn
+  res.locals.csrfToken = req.csrfToken()
+  next()
+})
+
+const authRoutes = require('./routes/auth');
+const adminRoutes = require('./routes/admin');
+
 app.use('/admin', adminRoutes);
+app.use(authRoutes);
 
 // app.use('/food-ar', frontEndController.getAR)
 
@@ -97,8 +131,16 @@ app.use(errorController.get404);
 //     console.log("HTTPS Server started at port 3000"); 
 //   });
 
+mongoose
+  .connect(MONGODB_URI)
+  .then(result => {
+    // app.listen(3000);
+    const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+    server.keepAliveTimeout = 120 * 1000;
+    server.headersTimeout = 120 * 1000;
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
