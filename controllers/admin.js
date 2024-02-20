f255d846e30fe8a189e40e277eece2147322f31d
Sarath <sarath.unnithan@gmail.com>
2024-02-20 18:00:38 +0530

    adding file val..

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,7 @@
 const Product = require("../models/product");
 const path = require('path')
+// import { v4 as uuidv4 } from 'uuid';
+const { v1: uuidv1 } = require('uuid');
 
 const identicon = require('identicon')
 const fs = require('fs')
@@ -222,7 +224,9 @@ exports.postAddProduct = (req, res, next) => {
   const image = req.files;
   const modelName = image.itemModel[0].filename;
 
-  let imageName = title;
+//   let imageName = title;
+// let imageName = uuidv1() + '-' + title
+    let imageName = new Date().getTime().toString() + '-' + title;
   const imageFileName = imageName + '.png'
  
 //   const imagePath = path.join("/", 'image', imageFileName)
@@ -354,7 +358,10 @@ exports.getEditProduct = (req, res, next) => {
 		const image = req.files;
 		const modelName = image.itemModel[0].filename;
 
-		let imageName = title;
+		// let imageName = title;
+		// let imageName = uuidv1() + '-' + title
+		let imageName = new Date().getTime().toString() + '-' + title;
+
 		const imageFileName = imageName + '.png'
  
 	  const imagePath = path.join("/", 'image', imageFileName)
